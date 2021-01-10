561d83272e5b8899db7156568c6bc7a601c7b05a
André Teixeira <andre.teixeira@objective.com.br>
2021-01-10 00:32:57 -0300

    refactoring

--- exercicio2/src/test/java/br/com/andre/exercicio2/Exercicio2Tests.java
+++ exercicio2/src/test/java/br/com/andre/exercicio2/Exercicio2Tests.java
@@ -3,8 +3,6 @@ package br.com.andre.exercicio2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
