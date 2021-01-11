c1e6af8dd123982828876f2e4d48a8e87de10663
André Teixeira <andre.teixeira@objective.com.br>
2021-01-11 10:03:38 -0300

    ZipCode

--- exercicio4/src/main/java/br/com/andre/exercicio4/business/User.java
+++ exercicio4/src/main/java/br/com/andre/exercicio4/business/User.java
@@ -7,5 +7,5 @@ import lombok.Data;
 @Data
 public class User {
     private final String name;
-    private final String zipCode;
+    private final ZipCode zipCode;
 }
---
+++ exercicio4/src/main/java/br/com/andre/exercicio4/business/ZipCode.java
@@ -0,0 +1,8 @@
+package br.com.andre.exercicio4.business;
+
+import lombok.Data;
+
+@Data
+public class ZipCode {
+    private String zipCode;
+}
