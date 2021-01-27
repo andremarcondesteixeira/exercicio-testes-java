22449b1ddcee07309d7911651df53d347e8233f7
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-01-26 21:49:47 -0300

    small refactoring

--- exercicio1/src/test/java/br/com/andre/exercicio1/MultiplesCalculator_Tests.java
+++ exercicio1/src/test/java/br/com/andre/exercicio1/MultiplesCalculator_Tests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Arrays;
